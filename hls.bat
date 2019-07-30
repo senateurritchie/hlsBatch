4b5ce27f1a341cad360e8c5f51ae3c63f3f62146
senateurritchie <senateurritchie@users.noreply.github.com>
2019-07-30 15:21:55 +0200

    Add files via upload

---
+++ hls.bat
@@ -0,0 +1,45 @@
+:: Ceci est mon programme ffmpeg pour generer automatiquement
+:: une playlist hls avec des bitrates differents
+
+@echo off
+title FFMPEG HLS FOR VOD - Zacharie Assagou
+color 1F
+	set duration=10
+
+	
+	::ffmpeg -hide_banner -y -i %filename% -vf scale=w=640:h=360:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time %duration% -hls_playlist_type vod  -b:v 800k -maxrate 856k -bufsize 1200k -b:a 96k -hls_segment_filename hls/360p_%%03d.ts hls/360p.m3u8 -vf scale=w=842:h=480:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time %duration% -hls_playlist_type vod -b:v 1400k -maxrate 1498k -bufsize 2100k -b:a 128k -hls_segment_filename hls/480p_%%03d.ts hls/480p.m3u8 -vf scale=w=1280:h=720:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time %duration% -hls_playlist_type vod -b:v 2800k -maxrate 2996k -bufsize 4200k -b:a 128k -hls_segment_filename hls/720p_%%03d.ts hls/720p.m3u8 -vf scale=w=1920:h=1080:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time %duration% -hls_playlist_type vod -b:v 5000k -maxrate 5350k -bufsize 7500k -b:a 192k -hls_segment_filename hls/1080p_%%03d.ts hls/1080p.m3u8
+
+	:: parcourir tous les fichiers dans un dossier
+	:: %%~xi = l'extension du fichier
+	:: %%~ni = le nom du fichier sans l'extension
+	:: %%~di = le lecteur sur lequel le fichier se trouve
+	:: %%~pi = le chemin d'accès du fichier comme path
+	:: %%~fi = le chemin d'accès complet du fichier comme volume/chemin.extention
+	:: %%~zi = la taille en octet du fichier
+
+	FOR %%i in (*.mp4) do (
+		:: nous parcourons uniquement les fichiers
+		set name=%%~ni
+		set extention=%%~xi
+		set dossier=%%~dpi
+		set d_a_creer=%dossier%/%name%
+		set fichier=%name%%extention%
+		
+		echo %d_a_creer%
+
+		if not exist %d_a_creer% (
+			MKDIR %d_a_creer%
+		)
+
+		:: on execute FFMPEG pour creer une vignette
+		ffmpeg -y -ss 10 -i "%fichier%" -vframes 1 -s 640x360 -q:v 2 "%d_a_creer%/poster.jpg"
+
+		:: on execute FFMPEG pour generer le flux hls
+		ffmpeg -hide_banner -y -i "%fichier%" -vf scale=w=640:h=360:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time %duration% -hls_playlist_type vod  -b:v 800k -maxrate 856k -bufsize 1200k -b:a 96k -hls_segment_filename "%d_a_creer%/360p_%%03d.ts" "%d_a_creer%/360p.m3u8" -vf scale=w=842:h=480:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time %duration% -hls_playlist_type vod -b:v 1400k -maxrate 1498k -bufsize 2100k -b:a 128k -hls_segment_filename "%d_a_creer%/480p_%%03d.ts" "%d_a_creer%/480p.m3u8" -vf scale=w=1280:h=720:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time %duration% -hls_playlist_type vod -b:v 2800k -maxrate 2996k -bufsize 4200k -b:a 128k -hls_segment_filename "%d_a_creer%/720p_%%03d.ts" "%d_a_creer%/720p.m3u8" -vf scale=w=1920:h=1080:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 -sc_threshold 0 -g 48 -keyint_min 48 -hls_time %duration% -hls_playlist_type vod -b:v 5000k -maxrate 5350k -bufsize 7500k -b:a 192k -hls_segment_filename "%d_a_creer%/1080p_%%03d.ts" "%d_a_creer%/1080p.m3u8"
+
+
+	)
+
+
+	
+pause > nul
